2223ed6bdd13628532dcf3ebe8cf61f99e8b866b
NikolaySimakov <n.simakoff@gmail.com>
2020-09-13 00:31:22 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,60 @@
+import handlers
+from aiogram import executor, types
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from data import config
+from loader import dp, db
+import filters
+import logging
+
+filters.setup(dp)
+
+user_message = 'Пользователь'
+admin_message = 'Админ'
+
+
+@dp.message_handler(commands='start')
+async def cmd_start(message: types.Message):
+
+    markup = ReplyKeyboardMarkup()
+
+    markup.row(user_message, admin_message)
+
+    await message.answer('''Привет! 👋
+
+🤖 Я бот-магазин по подаже товаров любой категории.
+    
+🛍️ Чтобы перейти в каталог и выбрать приглянувшиеся товары возпользуйтесь командой /menu.
+
+💰 Пополнить счет можно через Яндекс.кассу, Сбербанк или Qiwi.
+
+❓ Возникли вопросы? Не проблема! Команда /sos поможет связаться с админами, которые постараются как можно быстрее откликнуться.
+
+🤝 Заказать похожего бота? Свяжитесь с разработчиком <a href="https://t.me/NikolaySimakov">Nikolay Simakov</a>, он не кусается)))
+    ''', reply_markup=markup)
+
+
+@dp.message_handler(text=user_message)
+async def user_mode(message: types.Message):
+
+    cid = message.chat.id
+    if cid in config.ADMINS:
+        config.ADMINS.remove(cid)
+
+    await message.answer('Включен пользовательский режим.', reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message_handler(text=admin_message)
+async def admin_mode(message: types.Message):
+
+    cid = message.chat.id
+    if cid not in config.ADMINS:
+        config.ADMINS.append(cid)
+
+    await message.answer('Включен админский режим.', reply_markup=ReplyKeyboardRemove())
+
+
+async def on_startup(dp):
+    logging.basicConfig(level=logging.INFO)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=False)
