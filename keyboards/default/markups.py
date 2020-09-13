1a86603e881f46a2cfc53695a17c6d7d8d4c77ae
NikolaySimakov <45738826+NikolaySimakov@users.noreply.github.com>
2020-09-13 12:10:12 +0300

    All markups were resized in markups.py

--- keyboards/default/markups.py
+++ keyboards/default/markups.py
@@ -6,26 +6,26 @@ all_right_message = '✅ Все верно'
 cancel_message = '🚫 Отменить'
 
 def confirm_markup():
-    markup = ReplyKeyboardMarkup(selective=True)
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add(confirm_message)
     markup.add(back_message)
 
     return markup
 
 def back_markup():
-    markup = ReplyKeyboardMarkup(selective=True)
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add(back_message)
 
     return markup
 
 def check_markup():
-    markup = ReplyKeyboardMarkup(selective=True)
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.row(back_message, all_right_message)
 
     return markup
 
 def submit_markup():
-    markup = ReplyKeyboardMarkup(selective=True)
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.row(cancel_message, all_right_message)
 
     return markup
