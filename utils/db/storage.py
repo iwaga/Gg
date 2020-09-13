e80fbf42aad753a64147be31825b8453dc286f02
NikolaySimakov <45738826+NikolaySimakov@users.noreply.github.com>
2020-09-13 13:15:43 +0300

    Added create_tables method

--- utils/db/storage.py
+++ utils/db/storage.py
@@ -9,6 +9,14 @@ class DatabaseManager(object):
         self.conn.commit()
         self.cur = self.conn.cursor()
 
+    def create_tables(self):
+        self.query('CREATE TABLE IF NOT EXISTS products (idx text, title text, body text, photo blob, price int, tag text)')
+        self.query('CREATE TABLE IF NOT EXISTS orders (cid int, usr_name text, usr_address text, products text)')
+        self.query('CREATE TABLE IF NOT EXISTS cart (cid int, idx text, quantity int)')
+        self.query('CREATE TABLE IF NOT EXISTS categories (idx text, title text)')
+        self.query('CREATE TABLE IF NOT EXISTS wallet (cid int, balance real)')
+        self.query('CREATE TABLE IF NOT EXISTS questions (cid int, question text)')
+        
     def query(self, arg, values=None):
         if values == None:
             self.cur.execute(arg)
