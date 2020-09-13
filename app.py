459a3b98c9a7c1a8e72e2f99eeeb123a0e296d5a
NikolaySimakov <45738826+NikolaySimakov@users.noreply.github.com>
2020-09-13 17:14:24 +0300

    Webhook
    Webhook for Heroku.

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+
+import os
 import handlers
 from aiogram import executor, types
 from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from data import config
-from loader import dp, db
+from loader import dp, db, bot
 import filters
 import logging
 
 filters.setup(dp)
 
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = int(os.environ.get("PORT", 5000))
 user_message = 'Пользователь'
 admin_message = 'Админ'
 
@@ -57,5 +61,32 @@ async def on_startup(dp):
     logging.basicConfig(level=logging.INFO)
     db.create_tables()
 
+    await bot.delete_webhook()
+    await bot.set_webhook(config.WEBHOOK_URL)
+
+
+async def on_shutdown():
+    logging.warning("Shutting down..")
+    await bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+    logging.warning("Bot down")
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup, skip_updates=False)
+
+    if "HEROKU" in list(os.environ.keys()):
+
+        executor.start_webhook(
+            dispatcher=dp,
+            webhook_path=config.WEBHOOK_PATH,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            skip_updates=True,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT,
+        )
+
+    else:
+
+        executor.start_polling(dp, on_startup=on_startup, skip_updates=False)
