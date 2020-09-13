34800e3abc1fc341efdb921d014f879f534263ac
NikolaySimakov <45738826+NikolaySimakov@users.noreply.github.com>
2020-09-13 17:17:13 +0300

    Webhook data

--- data/config.py
+++ data/config.py
@@ -1,3 +1,9 @@
 BOT_TOKEN = '**********:**************************'
 
+PROJECT_NAME = 'store-bot-example'
+
+WEBHOOK_HOST = f"https://{PROJECT_NAME}.herokuapp.com"
+WEBHOOK_PATH = '/webhook/' + BOT_TOKEN
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
 ADMINS = [000000000, 1234567890]
