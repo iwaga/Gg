508bd8e4865432a033bfa87f6e2c0185544e59d6
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-08 06:14:07 +0300

    Add env example

--- data/config.py
+++ data/config.py
@@ -1,9 +1,22 @@
-BOT_TOKEN = '**********:**************************'
+from typing import List
+from environs import Env
 
-PROJECT_NAME = 'store-bot-example'
+env = Env()
+env.read_env()
 
-WEBHOOK_HOST = f"https://{PROJECT_NAME}.herokuapp.com"
-WEBHOOK_PATH = '/webhook/' + BOT_TOKEN
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+BOT_TOKEN: str = env.str("BOT_TOKEN")
+ADMINS: List[str] = env.str("ADMINS").split(',')
 
-ADMINS = [000000000, 1234567890]
+USE_PG: bool = env.bool("USE_PG", False) # if you use postgres
+
+PG_HOST: str = env.str("PG_HOST")
+PG_PORT: int = env.int("PG_PORT")
+PG_USER: str = env.str("PG_USER")
+PG_PASSWORD: str = env.str("PG_PASSWORD")
+PG_DATABASE: str = env.str("PG_DATABASE")
+
+USE_WEBHOOK: bool = env.bool("USE_WEBHOOK", False) # if you use webhook else pooling
+
+if USE_WEBHOOK:
+    MAIN_WEBHOOK_ADDRESS: str = env.str("MAIN_WEBHOOK_ADDRESS")
+    MAIN_WEBHOOK_SECRET_TOKEN: str = env.str("MAIN_WEBHOOK_SECRET_TOKEN")
