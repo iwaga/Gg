9058b6ea1003eebce3c693cede7d72f5d738813d
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-08 05:14:46 +0300

    Update imports of states & create language state

--- states/__init__.py
+++ states/__init__.py
@@ -1,3 +1,4 @@
 from .checkout_state import CheckoutState
 from .product_state import ProductState, CategoryState
 from .sos_state import SosState, AnswerState
+from .language_state import LanguageState
