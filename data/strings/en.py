14f5df75197ed8104a05a9b75a90c3563f646d1f
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-08 05:47:50 +0300

    Init i18n languages

---
+++ data/strings/en.py
@@ -0,0 +1,12 @@
+start_message = '''Hello! 👋
+
+🤖 I am a shop-bot for selling goods of any category.
+
+🛍️ To go to the catalog and choose the products you like, use the /menu command.
+
+💰 You can top up your account through PayPal, Yandex.Kassa.
+
+❓ Have any questions? No problem! The /sos command will help you contact the admins, who will try to respond as quickly as possible.
+
+🤝 Want to order a similar bot? Contact the developer <a href="https://t.me/NikolaySimakov">Nikolay Simakov</a>, he doesn't bite)))
+'''
